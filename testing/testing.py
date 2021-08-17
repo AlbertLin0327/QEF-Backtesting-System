ca74fb44eb3b4c96a3d7ee8d82b82a24efcf6200
dinochen1020 <dinochencool@gmail.com>
2021-08-17 21:10:18 +0800

    update search interval and fixed comment format

--- testing/testing.py
+++ testing/testing.py
@@ -3,20 +3,37 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-def fetch(file_path): # given file path and change it to two-dimensional data
-    # print(f"Reading {file_path}")
+def fetch(file_path): 
+    # Read the file and transform to pandas.data_form
     data = pq.read_table(source=file_path).to_pandas()
     return data
 
-def search(price_vol, year, month, day): # given year/month/day, return the price information
-    return price_vol[f"{year}-{month:02}-{day:02}"]
+def search(price_vol, year, month, day): 
+    # return the price information for the given date
+    try:
+        return price_vol[f"{year}-{month:02}-{day:02}"]
+    except:
+        print("invalid date")
+        return None
+
+def search_interval(price_vol, start, end):
+    # return all the information from start_date to end_date
+    data = []
+    for year in range(start[0], end[0] + 1):
+        for month in range(1, 13):
+            for day in range(1, 32):
+                if f'{start[0]}-{start[1]:02}-{start[2]:02}' <= f'{year}-{month:02}-{day:02}' <= f'{end[0]}-{end[1]:02}-{end[2]:02}':
+                    try:
+                        data.append(price_vol[f"{year}-{month:02}-{day:02}"])
+                    except:
+                        continue
+    return data
 
 def main():
     print('transforming parquet to data array.')
     mapping = []
     for i in range(4):
         mapping.append(fetch(f"../Dataset/mapping/part.{i}.parquet"))
-    print(mapping[0])
     price_vol = {}
     for year in range(1981, 2021):
         for month in range(1, 13):
